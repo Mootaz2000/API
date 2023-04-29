a71f637329776286f1b2c687d5fce98ebc048911
mootaz2000 <mootazbendhafer@gmail.com>
2023-04-29 15:39:18 +0100

    first commit

---
+++ first-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import UserCard from './UserCard';
+import UserList from './UserList';
+
+function App() {
+  return (
+   <>
+   
+  
+   <UserList/>
+   
+   </>
+        
+  );
+}
+
+export default App;
